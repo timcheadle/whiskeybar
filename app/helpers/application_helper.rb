a43520c2ac1adf6a1c2a55f0f233caf7bfc28877
Tim Cheadle <tim@rationalmeans.com>
2016-10-22 15:05:01 -0400

    Cleaned up Bootstrap views and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_heading(title, button_text = nil, button_url = nil)
+    content_for(:title, title)
+
+    content_tag(:div, class: "page-header") do
+      if button_text.present? && button_url.present?
+        concat link_to(button_text, button_url, class: "btn btn-primary float-xs-right")
+      end
+
+      concat content_tag(:h1, title)
+    end
+  end
 end
