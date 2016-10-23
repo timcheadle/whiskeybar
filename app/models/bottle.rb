0b796674ef6e2309860b52182891903f0013306d
Tim Cheadle <tim@rationalmeans.com>
2016-10-23 10:37:59 -0400

    Cleaned up validations to allow easier data import

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,6 +1,6 @@
 class Bottle < ApplicationRecord
   validates :name, presence: true
-  validates :producer, presence: true
+  #validates :producer, presence: true
   validates :spirit, presence: true
   validates :location, presence: true
   validates :acquired_on, presence: true
@@ -8,8 +8,7 @@ class Bottle < ApplicationRecord
   validates :volume, presence: true
   validates :volume, numericality: { greater_than: 0, only_integer: true }
 
-  validates :proof, presence: true
-  validates :proof, numericality: { greater_than: 0 }
+  validates :proof, numericality: { greater_than: 0, allow_nil: true }
 
-  validates :release_year, numericality: { greater_than: 0, only_integer: true }
+  validates :release_year, numericality: { greater_than: 0, only_integer: true, allow_nil: true }
 end
--- test/models/bottle_test.rb
+++ test/models/bottle_test.rb
@@ -10,7 +10,7 @@ class BottleTest < ActiveSupport::TestCase
   end
 
   should validate_presence_of(:name)
-  should validate_presence_of(:producer)
+  #should validate_presence_of(:producer)
   should validate_presence_of(:spirit)
   should validate_presence_of(:location)
   should validate_presence_of(:acquired_on)
@@ -18,8 +18,7 @@ class BottleTest < ActiveSupport::TestCase
   should validate_presence_of(:volume)
   should validate_numericality_of(:volume).is_greater_than(0).only_integer
 
-  should validate_presence_of(:proof)
-  should validate_numericality_of(:proof).is_greater_than(0)
+  should validate_numericality_of(:proof).is_greater_than(0).allow_nil
 
-  should validate_numericality_of(:release_year).is_greater_than(0).only_integer
+  should validate_numericality_of(:release_year).is_greater_than(0).only_integer.allow_nil
 end
