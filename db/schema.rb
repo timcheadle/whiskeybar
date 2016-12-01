87236243ba3bc05534d0ae9b471582e99bd4fa8a
Tim Cheadle <tim@rationalmeans.com>
2016-11-30 22:19:45 -0500

    Switch to using finished_on (#15)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201022415) do
+ActiveRecord::Schema.define(version: 20161201031647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20161201022415) do
     t.text     "notes"
     t.string   "source"
     t.boolean  "open"
-    t.boolean  "finished"
     t.string   "location"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
