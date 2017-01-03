a4104ee1d29431465315b2222b1cec3b1a14c9c6
Tim Cheadle <tim@rationalmeans.com>
2017-01-03 17:02:07 -0500

    Keep users logged in for 14 days

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :timeoutable,
+         :validatable
 
   has_many :bottles
 end
