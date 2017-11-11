660e031b5a15e25f8ec9e7207f633df12f90f7cf
Tim Cheadle <tim@rationalmeans.com>
2017-11-11 12:30:28 -0500

    Added tags and for sale UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :bottles do
     collection do
-      get "/:filter", to: "bottles#index", as: :filtered, constraints: { filter: /open|finished|stocked|unstocked/ }
+      get "/:filter", to: "bottles#index", as: :filtered, constraints: { filter: /open|finished|sell|stocked|unstocked/ }
       get "/search", to: "bottles#search", as: :search
       get "/toggle_inventory", to: "bottles#toggle_inventory", as: :toggle_inventory
       get "/reset_stock", to: "bottles#reset_stock", as: :reset_stock
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
       get "/unstock", to: "bottles#unstock", as: :unstock
       get "/open", to: "bottles#crack_open", as: :open
       get "/finish", to: "bottles#finish", as: :finish
+      get "/sell", to: "bottles#sell", as: :sell
     end
   end
 
